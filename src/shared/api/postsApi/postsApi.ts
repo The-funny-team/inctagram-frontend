20a83de3f7cfa86a98867d76801c3926c3184923
Aleksandrlitvinov <84243297+Aleksandrlitvinov@users.noreply.github.com>
2024-04-25 12:06:47 +0200

    I2-46 feat: add queries in postApi

--- src/shared/api/postsApi/postsApi.ts
+++ src/shared/api/postsApi/postsApi.ts
@@ -1,3 +1,44 @@
+import { baseApi } from '@/shared/api/baseApi'
+
+const postApi = baseApi.injectEndpoints({
+  endpoints: builder => ({
+    getPost: builder.query<PostResponseType, { id: string }>({
+      query: ({ id }) => {
+        return {
+          method: 'GET',
+          url: `post/${id}`,
+        }
+      },
+    }),
+    getPublicPost: builder.query<PostResponseType, { id: string }>({
+      query: ({ id }) => {
+        return {
+          method: 'GET',
+          url: `post/${id}`,
+        }
+      },
+    }),
+    getPublicPosts: builder.query<PostsResponse, GetPostsArgs>({
+      query: args => {
+        return {
+          method: 'GET',
+          params: args,
+          url: `/public/post`,
+        }
+      },
+    }),
+    getUserPosts: builder.query<PostsResponse, GetPostsArgs>({
+      query: args => {
+        return {
+          method: 'GET',
+          params: args,
+          url: `post`,
+        }
+      },
+    }),
+  }),
+})
+
 export type PostResponseType = {
   authorId: string
   createdAt: string
