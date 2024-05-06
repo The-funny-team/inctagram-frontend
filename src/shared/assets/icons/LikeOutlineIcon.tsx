575f6ddf591d9cd2469e8ef716bf8f03d2216e18
Igor Olkhov <smartestbox@gmail.com>
2024-05-07 01:49:06 +0200

    fix: add props and ref attributes to the like outline icon

--- src/shared/assets/icons/LikeOutlineIcon.tsx
+++ src/shared/assets/icons/LikeOutlineIcon.tsx
@@ -4,9 +4,11 @@ const SvgComponent = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) =
   <svg
     fill={'currentColor'}
     height={'24'}
+    ref={ref}
     viewBox={'0 0 24 24'}
     width={'24'}
     xmlns={'http://www.w3.org/2000/svg'}
+    {...props}
   >
     <g clipPath={'url(#clip0_301_4077)'}>
       <path
