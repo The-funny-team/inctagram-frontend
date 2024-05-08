5ee47bb7378c60cf53905ec3be2a76ec8bcae38c
Sergey <Zaikouski.sergey@gmail.com>
2024-05-08 11:13:20 +0300

    I2-41 feat add feature

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -5,44 +5,40 @@ import {useMeQuery} from "@/shared/api/profileApi";
 import React from "react";
 import Link from "next/link";
 import {ROUTES_URL} from "@/shared/const";
+import {UsersCountInfo} from "@/pages/profile/features/usersCountInfo";
+import {useTranslation} from "@/shared/lib/hooks";
 
 const ProfilePage = () => {
+    const {  text } = useTranslation()
+    const t = text.pages.profile.main
     const {data: userInfo} = useMeQuery()
     const avatar = userInfo?.avatarUrl
+    // const userId = userInfo?.id
+
 
     return (
         <>
             <HeadMeta title={'Profile'}/>
             <main className={s.rootPage}>
-                <div className={s.mainInfo} >
+                <div className={s.mainInfo}>
                     <Avatar className={s.avatarPhoto} size={204} src={avatar} userName={'my profile'}/>
                     <div className={s.infoAboutMe}>
                         <div className={s.nameAndBtn}>
-                            <Typography  variant={'h1'}> URLProfiel</Typography>
-                            <Button as={Link} href={ROUTES_URL.GENERAL_INFO} variant={'secondary'}>Profile Settings</Button>
+                            <Typography variant={'h1'}> {userInfo?.username}</Typography>
+                            <Button as={Link} href={ROUTES_URL.GENERAL_INFO} variant={'secondary'}>{t.profileSettings}</Button>
                         </div>
                         <div className={s.counting}>
-                            <div>
-                                <Typography variant={'boldText14'}>2 218</Typography>
-                                <Typography variant={'regularText14'}>Following</Typography>
-                            </div>
-                            <div>
-                                <Typography variant={'boldText14'}>2 218</Typography>
-                                <Typography variant={'regularText14'}>Followers</Typography>
-                            </div>
-                            <div>
-                                <Typography variant={'boldText14'}>2 218</Typography>
-                                <Typography variant={'regularText14'}>Publications</Typography>
-                            </div>
+                            <UsersCountInfo count={2218} name={t.following}/>
+                            <UsersCountInfo count={2218} name={t.followers}/>
+                            <UsersCountInfo count={2218} name={t.publications}/>
 
                         </div>
                         <div className={s.description}>
-                            <Typography variant={'regularText16'}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</Typography>
+                            <Typography variant={'regularText16'}>{userInfo?.aboutMe}</Typography>
                         </div>
                     </div>
                 </div>
                 <div className={s.postsList}></div>
-
             </main>
         </>
     )
