ef1fb4e62b57bc4179bbea4dd68f94a638e4fd08
Aleksandrlitvinov <84243297+Aleksandrlitvinov@users.noreply.github.com>
2024-05-13 17:25:44 +0200

    I2-31 feat: add request to upload image

--- src/widgets/CreatePost/service/getFilteredImage.ts
+++ src/widgets/CreatePost/service/getFilteredImage.ts
@@ -1,7 +1,13 @@
+import { AppDispatch } from '@/application/store'
+import { FileUploadResponse } from '@/shared/api/postsApi'
 import { createImageElement } from '@/widgets/CreatePost/service/createImageElement'
-import { CroppedPicture } from '@/widgets/CreatePost/service/createPost.slice'
+import { CroppedPicture, setPicturesIds } from "@/widgets/CreatePost/service/createPost.slice";
 
-export const getFilteredImage = async (croppedPictureObj: CroppedPicture): Promise<string> => {
+export const getFilteredImage = async (
+  croppedPictureObj: CroppedPicture,
+  dispatch: AppDispatch,
+  uploadToServer: any // Todo: add type
+): Promise<string> => {
   const imageObj = await createImageElement(croppedPictureObj.img)
   const canvasObj = document.createElement('canvas')
   const ctx = canvasObj.getContext('2d')
@@ -18,15 +24,27 @@ export const getFilteredImage = async (croppedPictureObj: CroppedPicture): Promi
 
   const dataUrl = canvasObj.toDataURL('image/jpeg')
 
-  // canvasObj.toBlob(blob => {
-  //   if (blob) {
-  //     const timestamp = Date.now()
-  //     const fileName = `photo_${timestamp}.png`
-  //     const file = new File([blob], fileName, { type: 'image/png' })
-  //
-  //    //make request
-  //   }
-  // }, 'image/jpeg')
+  canvasObj.toBlob(async blob => {
+    if (blob) {
+      const timestamp = Date.now()
+      const fileName = `photo_${timestamp}.png`
+      const file = new File([blob], fileName, { type: 'image/png' })
+      const formData = new FormData()
+
+      formData.append('image', file)
+
+      try {
+        const response = await uploadToServer(formData)
+        const imageId = response?.data?.fileId
+
+        if (imageId) {
+          dispatch(setPicturesIds(imageId))
+        }
+      } catch (error: unknown) {
+        console.error('Ошибка загрузки на сервер:', error)
+      }
+    }
+  }, 'image/jpeg')
 
   imageObj.remove()
   canvasObj.remove()
