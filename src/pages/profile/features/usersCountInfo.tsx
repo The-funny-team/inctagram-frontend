ca9ba8b4df67ec3bb1892c2c135d732598d60dd2
Sergey <Zaikouski.sergey@gmail.com>
2024-05-23 12:54:30 +0300

    I2-41-2 fix: fix styles

--- src/pages/profile/features/usersCountInfo.tsx
+++ src/pages/profile/features/usersCountInfo.tsx
@@ -1,17 +1,18 @@
-import {Typography} from "@/shared/ui";
-import React from "react";
-type Props={
-    name: string
-    count: number
+import React from 'react'
+
+import { Typography } from '@/shared/ui'
+type Props = {
+  count: number
+  name: string
 }
 
-export const UsersCountInfo = ({count, name}:Props) => {
-    const number =count.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
-    return (
-        <div>
-            <Typography variant={'boldText14'}>{number}</Typography>
-            <Typography variant={'regularText14'}>{name}</Typography>
-        </div>
-    );
-};
+export const UsersCountInfo = ({ count, name }: Props) => {
+  const number = count.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ')
 
+  return (
+    <div>
+      <Typography variant={'boldText14'}>{number}</Typography>
+      <Typography variant={'regularText14'}>{name}</Typography>
+    </div>
+  )
+}
