92c4b11cba4421b7590275e6b6f8a4a70aaa0895
Aleksandrlitvinov <84243297+Aleksandrlitvinov@users.noreply.github.com>
2024-06-19 16:38:13 +0200

    I2-54 ref: remove useTranslation on home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,7 @@
 import { getRootLayout } from '@/shared/layouts'
-import { useTranslation } from '@/shared/lib/hooks'
 import { TotalUsersCounter } from '@/widgets/TotalUsersCounter'
 
 const Home = () => {
-  const { text } = useTranslation()
-
   return (
     <main>
       <TotalUsersCounter />
