cb038ba7e016807c2c6da592c99cb8ccf75ee321
Aleksandrlitvinov <84243297+Aleksandrlitvinov@users.noreply.github.com>
2024-06-22 21:59:27 +0200

    I2-54 ref: edit request name

--- src/widgets/TotalUsersCounter/TotalUsersCounter.tsx
+++ src/widgets/TotalUsersCounter/TotalUsersCounter.tsx
@@ -1,11 +1,11 @@
-import { useGetTotalUsersCountQuery } from '@/shared/api/profileApi'
+import { useGetUsersCountQuery } from '@/shared/api/profileApi'
 import { useTranslation } from '@/shared/lib/hooks'
 import { Typography } from '@/shared/ui'
 
 import s from './TotalUsersCounter.module.scss'
 
 export const TotalUsersCounter = () => {
-  const { data: usersCount } = useGetTotalUsersCountQuery()
+  const { data: usersCount } = useGetUsersCountQuery()
   const totalUsersCount = usersCount?.totalCount
   const { text } = useTranslation()
   const t = text.pages.home
