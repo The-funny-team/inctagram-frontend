8d1dd1d64991b45f01253a6543a03f3bc18c1b95
Aleksandrlitvinov <84243297+Aleksandrlitvinov@users.noreply.github.com>
2024-07-08 21:36:04 +0200

    I2-54 feat: create HeaderProfile component

---
+++ src/shared/ui/ProfileHeader/ProfileHeader.tsx
@@ -0,0 +1,45 @@
+import { User } from '@/shared/api/profileApi'
+import { ROUTES_URL } from '@/shared/const'
+import { useTranslation } from '@/shared/lib/hooks'
+import { Avatar, Button, Typography } from '@/shared/ui'
+import Link from 'next/link'
+
+import s from './ProfileHeader.module.scss'
+
+import { UserButtons, UsersCountInfo } from './features'
+
+type PropsType = {
+  totalPostsCount: number
+  user: User
+}
+
+export const ProfileHeader = ({ totalPostsCount, user }: PropsType) => {
+  const { text } = useTranslation()
+  const t = text.pages.profile.main
+
+  return (
+    <div className={s.mainInfo}>
+      <Avatar className={s.avatarPhoto} size={204} src={user.avatarUrl} userName={'my profile'} />
+      <div className={s.infoAboutMe}>
+        <div className={s.nameAndBtn}>
+          <Typography variant={'h1'}> {user?.username}</Typography>
+          {user ? (
+            <Button as={Link} href={ROUTES_URL.GENERAL_INFO} variant={'secondary'}>
+              {t.profileSettings}
+            </Button>
+          ) : (
+            <UserButtons />
+          )}
+        </div>
+        <div className={s.counting}>
+          <UsersCountInfo count={2232} name={t.following} />
+          <UsersCountInfo count={2311} name={t.followers} />
+          <UsersCountInfo count={totalPostsCount} name={t.publications} />
+        </div>
+        <div className={s.description}>
+          <Typography variant={'regularText16'}>{user?.aboutMe}</Typography>
+        </div>
+      </div>
+    </div>
+  )
+}
