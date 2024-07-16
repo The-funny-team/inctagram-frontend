12b6f4ab6d9bef6d1a455cf22ddd3675970ce8d6
Aleksandrlitvinov <84243297+Aleksandrlitvinov@users.noreply.github.com>
2024-07-16 22:37:45 +0200

    I2-54 feat: add component PublicPosts

---
+++ src/shared/ui/PublicPosts/PublicPosts.tsx
@@ -0,0 +1,26 @@
+import type { GetPostResponse } from '@/shared/api/postsApi'
+
+import { PublicPost } from '@/widgets/PublicPost'
+
+import s from './PublicPosts.module.scss'
+
+type Props = {
+  publicPosts: GetPostResponse[]
+}
+export const PublicPosts = ({ publicPosts }: Props) => {
+  return (
+    <div className={s.postsWrapper}>
+      {publicPosts?.map(p => (
+        <PublicPost
+          author={p.author}
+          createdAt={p.createdAt}
+          description={p.description}
+          id={p.author.id}
+          imagesUrl={p.imagesUrl}
+          key={p.id}
+          updatedAt={p.updatedAt}
+        />
+      ))}
+    </div>
+  )
+}
