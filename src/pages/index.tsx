006cb80225486213eaca6490a514c41c7093ec84
Aleksandrlitvinov <84243297+Aleksandrlitvinov@users.noreply.github.com>
2024-07-17 10:29:43 +0200

    I2-54 feat: add getStaticProps in PublicPage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,49 @@
+import { GetPostsResponse } from '@/shared/api/postsApi'
+import { BASE_API_URL } from '@/shared/const'
 import { getRootLayout } from '@/shared/layouts'
+import { HeadMeta, Loader, PublicPosts, RedirectToProfilePage } from '@/shared/ui'
 import { TotalUsersCounter } from '@/widgets/TotalUsersCounter'
+import { GetStaticProps } from 'next'
 
-const Home = () => {
+export const getStaticProps: GetStaticProps = async () => {
+  const res = await fetch(`${BASE_API_URL}/public/post?take=4`)
+  const posts: GetPostsResponse = await res.json()
+
+  if (!posts) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {
+      publicPosts: posts,
+    },
+    revalidate: 60,
+  }
+}
+
+type PropsType = {
+  publicPosts: GetPostsResponse
+}
+
+const PublicPage = ({ publicPosts }: PropsType) => {
   return (
-    <main>
-      <TotalUsersCounter />
-    </main>
+    <>
+      <HeadMeta title={'public page'} />
+      <RedirectToProfilePage>
+        {publicPosts?.data ? (
+          <main style={{ margin: '0 auto', maxWidth: '972px' }}>
+            <TotalUsersCounter />
+            {publicPosts && <PublicPosts publicPosts={publicPosts.data} />}
+          </main>
+        ) : (
+          <Loader />
+        )}
+      </RedirectToProfilePage>
+    </>
   )
 }
 
-Home.getLayout = getRootLayout
-export default Home
+PublicPage.getLayout = getRootLayout
+export default PublicPage
