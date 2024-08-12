a5216e49776cdceb96728ed6dcb2a74f28c01c55
Aleksandrlitvinov <84243297+Aleksandrlitvinov@users.noreply.github.com>
2024-08-12 22:28:21 +0200

    FUN-21 ref: add Loader

--- src/widgets/Payments/ui/PaymentsListTable.tsx
+++ src/widgets/Payments/ui/PaymentsListTable.tsx
@@ -1,5 +1,6 @@
 import { useGetPaymentsQuery } from '@/shared/api/paymentApi'
 import { useTranslation } from '@/shared/lib/hooks'
+import { Loader } from '@/shared/ui'
 import {
   Table,
   TableBody,
@@ -13,36 +14,42 @@ import {
 export const PaymentsListTable = () => {
   const { text } = useTranslation()
   const t = text.pages.profile.myPayments
-  const { data: payments } = useGetPaymentsQuery()
+  const { data: payments, isLoading } = useGetPaymentsQuery()
 
-  if (!payments?.length) {
-    return <TableEmpty />
+  if (isLoading) {
+    return <Loader />
   }
 
   return (
-    <Table>
-      <TableHead>
-        <TableRow>
-          <TableHeadCell>{t.dateOfPayment}</TableHeadCell>
-          <TableHeadCell>{t.endDateOfSubscription}</TableHeadCell>
-          <TableHeadCell>{t.price}</TableHeadCell>
-          <TableHeadCell>{t.subscriptionType}</TableHeadCell>
-          <TableHeadCell>{t.paymentType}</TableHeadCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {payments?.map(payment => (
-          <TableRow key={payment.userId}>
-            <TableCell>{new Date(payment.dateOfPayment).toLocaleDateString('ru-RU')}</TableCell>
-            <TableCell>
-              {new Date(payment.endDateOfSubscription).toLocaleDateString('ru-RU')}
-            </TableCell>
-            <TableCell>${payment.price}</TableCell>
-            <TableCell>{payment.subscriptionType}</TableCell>
-            <TableCell>{payment.paymentType}</TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+    <>
+      {payments && payments.length ? (
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableHeadCell>{t.dateOfPayment}</TableHeadCell>
+              <TableHeadCell>{t.endDateOfSubscription}</TableHeadCell>
+              <TableHeadCell>{t.price}</TableHeadCell>
+              <TableHeadCell>{t.subscriptionType}</TableHeadCell>
+              <TableHeadCell>{t.paymentType}</TableHeadCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {payments?.map(payment => (
+              <TableRow key={payment.userId}>
+                <TableCell>{new Date(payment.dateOfPayment).toLocaleDateString('ru-RU')}</TableCell>
+                <TableCell>
+                  {new Date(payment.endDateOfSubscription).toLocaleDateString('ru-RU')}
+                </TableCell>
+                <TableCell>${payment.price}</TableCell>
+                <TableCell>{payment.subscriptionType}</TableCell>
+                <TableCell>{payment.paymentType}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      ) : (
+        <TableEmpty />
+      )}
+    </>
   )
 }
