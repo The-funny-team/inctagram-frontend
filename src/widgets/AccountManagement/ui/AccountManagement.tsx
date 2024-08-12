d388bd3ba51f2ab05853c0bebd388e244d7905fe
Aleksandrlitvinov <84243297+Aleksandrlitvinov@users.noreply.github.com>
2024-08-13 00:23:37 +0200

    FUN-21 feat: get current subscription & check its validity

--- src/widgets/AccountManagement/ui/AccountManagement.tsx
+++ src/widgets/AccountManagement/ui/AccountManagement.tsx
@@ -1,16 +1,25 @@
+import { useGetCurrentPaymentSubscriptionsQuery } from '@/shared/api/paymentApi'
+
 import s from './AccountManagement.module.scss'
 
 import { ChoiceSubscription } from './ChoiceSubscription'
 import { CurrentSubscription } from './CurrentSubscription'
 
 export const AccountManagement = () => {
-  const isCurrentSubscriptions = true // request current subscriptions
+  const { data: currentSubscription } = useGetCurrentPaymentSubscriptionsQuery()
 
-  //
+  if (!currentSubscription?.data) {
+    return null
+  }
+
+  const curSubscription = currentSubscription.data[0]
+
+  const isValidCurSubscription =
+    new Date().getTime() < new Date(curSubscription.endDateOfSubscription).getTime()
 
   return (
     <main className={s.root}>
-      {isCurrentSubscriptions && <CurrentSubscription />}
+      {isValidCurSubscription && <CurrentSubscription currentSubscription={curSubscription} />}
       <ChoiceSubscription />
     </main>
   )
--- src/widgets/AccountManagement/ui/CurrentSubscription/CurrentSubscription.tsx
+++ src/widgets/AccountManagement/ui/CurrentSubscription/CurrentSubscription.tsx
@@ -1,28 +1,33 @@
-import { useState } from 'react'
-
+import { CurrentPaymentSubscription } from '@/shared/api/paymentApi'
 import { Card, Checkbox, Typography } from '@/shared/ui'
 
 import s from './CurrentSubscription.module.scss'
 
 import { ShowDate } from './ShowDate'
 
-export const CurrentSubscription = () => {
-  const [isShowNextPayment, setShowNextPayment] = useState(false)
+type PropsType = {
+  currentSubscription: CurrentPaymentSubscription
+}
 
-  const showNextPayment = () => {
-    setShowNextPayment(!isShowNextPayment)
-  }
+export const CurrentSubscription = ({ currentSubscription }: PropsType) => {
+  const { autoRenewal, dateOfPayment, endDateOfSubscription } = currentSubscription
 
   return (
     <div className={s.currentSubscription}>
       <Typography variant={'h3'}>Current Subscription:</Typography>
       <Card style={{ padding: '12px 24px' }}>
         <div className={s.info}>
-          <ShowDate date={'12.12.2022'} headerText={'Expire at'} />
-          {isShowNextPayment && <ShowDate date={'12.12.2022'} headerText={'Next payment'} />}
+          <ShowDate
+            date={new Date(endDateOfSubscription).toLocaleDateString('ru-RU')}
+            headerText={'Expire at'}
+          />
+          <ShowDate
+            date={new Date(dateOfPayment).toLocaleDateString('ru-RU')}
+            headerText={'Next payment'}
+          />
         </div>
       </Card>
-      <Checkbox label={'Auto-Renewal'} onCheckedChange={showNextPayment} />
+      <Checkbox checked={autoRenewal} label={'Auto-Renewal'} />
     </div>
   )
 }
