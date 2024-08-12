be114c8ef7e376aba8af5caa2673b10273473358
Aleksandrlitvinov <84243297+Aleksandrlitvinov@users.noreply.github.com>
2024-08-12 23:02:24 +0200

    FUN-21 ref: get subscriptions in Payments & add styles

--- src/widgets/Payments/ui/Payments.tsx
+++ src/widgets/Payments/ui/Payments.tsx
@@ -1,6 +1,9 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 
+import { useGetPaymentsQuery } from '@/shared/api/paymentApi'
+import { Loader } from '@/shared/ui'
 import { Pagination } from '@/shared/ui/Pagination'
+import { TableEmpty } from '@/shared/ui/Table'
 import { PaymentsListTable } from '@/widgets/Payments/ui/PaymentsListTable'
 
 import s from './Payments.module.scss'
@@ -19,21 +22,34 @@ const PAGINATION_OPTIONS: Option[] = [
 export const Payments = () => {
   const [currentPage, setCurrentPage] = useState<number>(1)
   const [pageSize, setPageSize] = useState<number>(10)
+  const { data: payments, isLoading } = useGetPaymentsQuery()
 
   const handlePageSize = (pageSize: string) => {
     setPageSize(+pageSize)
   }
 
+  if (isLoading) {
+    return <Loader />
+  }
+
   return (
     <div className={s.container}>
-      <PaymentsListTable />
-      <Pagination
-        currentPage={currentPage}
-        onChangePage={setCurrentPage}
-        onValueChange={handlePageSize}
-        options={PAGINATION_OPTIONS}
-        pageSize={pageSize}
-      />
+      {payments && payments.length ? (
+        <div className={s.paymentsTable}>
+          <PaymentsListTable payments={payments} />
+          <Pagination
+            className={s.pagination}
+            currentPage={currentPage}
+            onChangePage={setCurrentPage}
+            onValueChange={handlePageSize}
+            options={PAGINATION_OPTIONS}
+            pageSize={pageSize}
+            totalCount={payments.length}
+          />
+        </div>
+      ) : (
+        <TableEmpty />
+      )}
     </div>
   )
 }
--- src/widgets/Payments/ui/PaymentsListTable.tsx
+++ src/widgets/Payments/ui/PaymentsListTable.tsx
@@ -1,55 +1,39 @@
-import { useGetPaymentsQuery } from '@/shared/api/paymentApi'
+import { Payment } from '@/shared/api/paymentApi'
 import { useTranslation } from '@/shared/lib/hooks'
-import { Loader } from '@/shared/ui'
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableEmpty,
-  TableHead,
-  TableHeadCell,
-  TableRow,
-} from '@/shared/ui/Table'
+import { Table, TableBody, TableCell, TableHead, TableHeadCell, TableRow } from '@/shared/ui/Table'
 
-export const PaymentsListTable = () => {
+type PropsType = {
+  payments: Payment[]
+}
+
+export const PaymentsListTable = ({ payments }: PropsType) => {
   const { text } = useTranslation()
   const t = text.pages.profile.myPayments
-  const { data: payments, isLoading } = useGetPaymentsQuery()
-
-  if (isLoading) {
-    return <Loader />
-  }
 
   return (
-    <>
-      {payments && payments.length ? (
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableHeadCell>{t.dateOfPayment}</TableHeadCell>
-              <TableHeadCell>{t.endDateOfSubscription}</TableHeadCell>
-              <TableHeadCell>{t.price}</TableHeadCell>
-              <TableHeadCell>{t.subscriptionType}</TableHeadCell>
-              <TableHeadCell>{t.paymentType}</TableHeadCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {payments?.map(payment => (
-              <TableRow key={payment.userId}>
-                <TableCell>{new Date(payment.dateOfPayment).toLocaleDateString('ru-RU')}</TableCell>
-                <TableCell>
-                  {new Date(payment.endDateOfSubscription).toLocaleDateString('ru-RU')}
-                </TableCell>
-                <TableCell>${payment.price}</TableCell>
-                <TableCell>{payment.subscriptionType}</TableCell>
-                <TableCell>{payment.paymentType}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      ) : (
-        <TableEmpty />
-      )}
-    </>
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableHeadCell>{t.dateOfPayment}</TableHeadCell>
+          <TableHeadCell>{t.endDateOfSubscription}</TableHeadCell>
+          <TableHeadCell>{t.price}</TableHeadCell>
+          <TableHeadCell>{t.subscriptionType}</TableHeadCell>
+          <TableHeadCell>{t.paymentType}</TableHeadCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {payments?.map(payment => (
+          <TableRow key={payment.userId}>
+            <TableCell>{new Date(payment.dateOfPayment).toLocaleDateString('ru-RU')}</TableCell>
+            <TableCell>
+              {new Date(payment.endDateOfSubscription).toLocaleDateString('ru-RU')}
+            </TableCell>
+            <TableCell>${payment.price}</TableCell>
+            <TableCell>{payment.subscriptionType}</TableCell>
+            <TableCell>{payment.paymentType}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
   )
 }
