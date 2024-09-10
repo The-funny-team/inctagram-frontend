610d9443c39e06e2617e42d5d6e5de0e48430201
Sergey <Zaikouski.sergey@gmail.com>
2024-09-10 11:31:17 +0300

    FUN-29 feat: add button "DELETE USER" (temporary solution)

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,26 @@
+import { useDeleteUserMutation } from '@/shared/api/profileApi'
 import { getProtectedNavbarLayout } from '@/shared/layouts'
-import { HeadMeta } from '@/shared/ui'
+import { Button, HeadMeta } from '@/shared/ui'
 import { EmailVerification } from '@/widgets/EmailVerification/ui/EmailVerification'
 
-const HomePage = () => (
-  <>
-    <HeadMeta title={'Home'} />
-    <main>Home Page</main>
-  </>
-)
+const HomePage = () => {
+  const [deleteMe] = useDeleteUserMutation()
+  const deleteMeHandler = () => {
+    deleteMe()
+  }
+
+  return (
+    <>
+      <HeadMeta title={'Home'} />
+      <main>
+        Home Page
+        <div>
+          <Button onClick={deleteMeHandler}>delete me</Button>
+        </div>
+      </main>
+    </>
+  )
+}
 
 HomePage.getLayout = getProtectedNavbarLayout
 export default HomePage
