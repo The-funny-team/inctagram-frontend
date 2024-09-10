f310631c9fc185deb9e66252d9d65b8ac7cf0f25
Sergey <Zaikouski.sergey@gmail.com>
2024-09-10 11:34:22 +0300

    FUN-29 fix: fix code

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,6 @@
 import { useDeleteUserMutation } from '@/shared/api/profileApi'
 import { getProtectedNavbarLayout } from '@/shared/layouts'
 import { Button, HeadMeta } from '@/shared/ui'
-import { EmailVerification } from '@/widgets/EmailVerification/ui/EmailVerification'
 
 const HomePage = () => {
   const [deleteMe] = useDeleteUserMutation()
