196bd443dadab96186e83a189f6e3f6284718575
Sergey <Zaikouski.sergey@gmail.com>
2024-12-10 17:44:04 +0200

    FUN-98 feat: add infinity scroll

--- src/widgets/ProfileMain/ui/ProfileMain.tsx
+++ src/widgets/ProfileMain/ui/ProfileMain.tsx
@@ -1,5 +1,7 @@
+import { useEffect, useState } from 'react'
+
 import { useMeQuery } from '@/shared/api/authApi'
-import { useGetUserPostsQuery } from '@/shared/api/postsApi'
+import { GetPostResponse, useGetUserPostsQuery } from '@/shared/api/postsApi'
 import { useGetProfileInfoQuery } from '@/shared/api/profileApi'
 import { Loader, ProfileHeader } from '@/shared/ui'
 import { ViewPostModal } from '@/widgets/ViewPostModal'
@@ -9,8 +11,42 @@ import s from './ProfileMain.module.scss'
 export const ProfileMain = () => {
   const { data: userInfo } = useMeQuery()
   const { data: profileInfo } = useGetProfileInfoQuery()
-  const myId = userInfo?.userId
-  const { data: profilePosts, isLoading } = useGetUserPostsQuery({ userId: myId as number })
+  const myId = Number(userInfo?.userId)
+
+  const [endCursorPostId, setEndCursorPostId] = useState(0)
+  const [allPosts, setAllPosts] = useState<GetPostResponse[]>([] as GetPostResponse[])
+  const { data: profilePosts, isLoading } = useGetUserPostsQuery({
+    endCursorPostId: endCursorPostId,
+    pageSize: 8,
+    userId: myId,
+  })
+
+  useEffect(() => {
+    if (profilePosts && profilePosts.items.length !== 0) {
+      setAllPosts(prevPosts => [...prevPosts, ...profilePosts.items])
+    }
+  }, [profilePosts])
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = document.documentElement.scrollTop
+      const windowHeight = window.innerHeight
+      const fullHeight = document.documentElement.scrollHeight
+
+      if (windowHeight + scrollTop >= fullHeight - 100) {
+        if (allPosts.length > 0) {
+          const lastId = allPosts[allPosts.length - 1].id
+
+          setEndCursorPostId(lastId)
+        }
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [profilePosts, allPosts])
 
   const authorId =
     profilePosts && profilePosts.items.length !== 0 ? profilePosts.items[0].ownerId : ''
@@ -38,26 +74,24 @@ export const ProfileMain = () => {
             user={profileInfo}
           />
           <div className={s.postsList}>
-            {profilePosts &&
-              profilePosts.items.map(post => (
-                <ViewPostModal
-                  avatarOwner={post.avatarOwner}
-                  comments={[]}
-                  createdAt={post.createdAt}
-                  description={post.description}
-                  id={post.id}
-                  images={post.images}
-                  isLiked
-                  isMyPost={isMyPost}
-                  key={post.id}
-                  likesCount={3}
-                  location={''}
-                  owner={post.owner}
-                  ownerId={post.ownerId}
-                  updatedAt={post.updatedAt}
-                  userName={post.userName}
-                />
-              ))}
+            {allPosts.map(post => (
+              <ViewPostModal
+                avatarOwner={post.avatarOwner}
+                comments={[]}
+                createdAt={post.createdAt}
+                description={post.description}
+                id={post.id}
+                images={post.images}
+                isLiked
+                key={post.id}
+                likesCount={3}
+                location={''}
+                owner={post.owner}
+                ownerId={post.ownerId}
+                updatedAt={post.updatedAt}
+                userName={post.userName}
+              />
+            ))}
           </div>
         </main>
       ) : (
