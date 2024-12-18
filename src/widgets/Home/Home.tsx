1b3b98fd477da23d9a5df98106fc0f6c161efbf3
Aleksandrlitvinov <84243297+Aleksandrlitvinov@users.noreply.github.com>
2024-12-18 23:02:52 +0100

    FUN-98-1 feat: add infinity scroll in Home page

--- src/widgets/Home/Home.tsx
+++ src/widgets/Home/Home.tsx
@@ -1,22 +1,58 @@
-import { useGetPublicationsFollowersQuery } from '@/shared/api/postsApi'
+import { useEffect, useState } from 'react'
+
+import {
+  GetPublicationsResponseItem,
+  useGetPublicationsFollowersQuery,
+} from '@/shared/api/postsApi'
 import { Loader } from '@/shared/ui'
 
 import { HomePost } from './HomePost'
 
 export const Home = () => {
-  const { data, isLoading } = useGetPublicationsFollowersQuery({})
+  const [endCursorPostId, setEndCursorPostId] = useState(0)
+  const [allPublications, setAllPublications] = useState<GetPublicationsResponseItem[]>([])
+  const { data: publications, isLoading } = useGetPublicationsFollowersQuery({
+    endCursorPostId,
+    pageSize: 12,
+  })
+
+  useEffect(() => {
+    if (publications && publications.items.length !== 0) {
+      setAllPublications(prevPosts => [...prevPosts, ...publications.items])
+    }
+  }, [publications])
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = document.documentElement.scrollTop
+      const windowHeight = window.innerHeight
+      const fullHeight = document.documentElement.scrollHeight
+
+      if (windowHeight + scrollTop >= fullHeight - 100) {
+        if (publications) {
+          setEndCursorPostId(publications.nextCursor)
+        }
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [publications, allPublications])
 
   if (isLoading) {
     return <Loader />
   }
 
-  if (!data) {
-    return
+  if (!publications) {
+    return null
   }
 
   return (
     <div style={{ paddingLeft: '175px', paddingTop: '30px' }}>
-      {data.items.map(publication => (
+      {allPublications.map(publication => (
         <HomePost key={publication.id} post={publication} />
       ))}
     </div>
