9ff2651c264b4f9b7460a596972c440622f014cb
Sergey <Zaikouski.sergey@gmail.com>
2025-01-21 20:06:27 +0200

    FUN-105 feat: add requests

--- src/widgets/ViewPostModal/ui/Actions/Actions.tsx
+++ src/widgets/ViewPostModal/ui/Actions/Actions.tsx
@@ -1,21 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import { FavoritesIcon, LikeOutlineIcon, ShareIcon } from '@/shared/assets'
+import { useUpdateLikeStatusPostMutation } from '@/shared/api/postsApi'
+import { FavoritesIcon, LikeIcon, LikeOutlineIcon, MessageIcon, ShareIcon } from '@/shared/assets'
+import { ROUTES_URL } from '@/shared/const'
+import { useRouter } from 'next/router'
 
 import s from './Actions.module.scss'
 
-export const Actions = () => {
+type Props = {
+  isLiked: boolean
+  isMyPost: boolean
+  postId: number
+}
+
+export const Actions = ({ isLiked, isMyPost, postId }: Props) => {
+  const router = useRouter()
+  const [isLikedPost, setIsLikedPost] = useState(isLiked)
+
+  const [updateLikeStatus] = useUpdateLikeStatusPostMutation()
+  const toggleLikeHandler = () => {
+    updateLikeStatus({ likeStatus: isLiked ? 'NONE' : 'LIKE', postId: postId })
+      .unwrap()
+      .then(() => {
+        setIsLikedPost(prevState => !prevState)
+      })
+  }
+  const onClickMessage = () => {
+    void router.push(ROUTES_URL.MESSENGER)
+  }
+
   return (
     <div className={s.actions}>
       <div className={s.likeAndShareContainer}>
-        <span className={s.likePostIcon}>
-          <LikeOutlineIcon />
-        </span>
-        <span className={s.sharePostIcon}>
+        <span onClick={toggleLikeHandler}>{isLikedPost ? <LikeIcon /> : <LikeOutlineIcon />}</span>
+        {!isMyPost && (
+          <span onClick={onClickMessage}>
+            <MessageIcon />
+          </span>
+        )}
+        <span>
           <ShareIcon />
         </span>
       </div>
-      <span className={s.favoritesIcon}>
+      <span>
         <FavoritesIcon />
       </span>
     </div>
