45799308b8af6d6d95b1c4510a0eb5e46f487f66
Aleksandrlitvinov <84243297+Aleksandrlitvinov@users.noreply.github.com>
2025-02-03 01:43:06 +0100

    FUN-110 ref: edit name story

--- src/shared/ui/ChatHeader/ChatHeader.stories.tsx
+++ src/shared/ui/ChatHeader/ChatHeader.stories.tsx
@@ -11,7 +11,7 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const Default: Story = {
+export const WithUserData: Story = {
   args: {
     avatarUrl:
       'https://staging-it-incubator.s3.eu-central-1.amazonaws.com/trainee-instagram-api/Image/4164019e-a863-4222-ab5b-e2657e8a8bb2_users/848/avatar/c309d797-46fe-4711-89a3-0349d083982f-images-192x192',
