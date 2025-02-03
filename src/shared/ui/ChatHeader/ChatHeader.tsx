27b1dc9fc18be5c00d172fb5fdb3171efad5f4da
Aleksandrlitvinov <84243297+Aleksandrlitvinov@users.noreply.github.com>
2025-02-03 20:38:32 +0100

    FUN-110 ref: remove conditional rendering

--- src/shared/ui/ChatHeader/ChatHeader.tsx
+++ src/shared/ui/ChatHeader/ChatHeader.tsx
@@ -10,12 +10,10 @@ type PropsType = {
 export const ChatHeader = ({ avatarUrl, userName }: PropsType) => {
   return (
     <div className={s.headerWrapper}>
-      {userName && avatarUrl && (
-        <div className={s.userInfo}>
-          <Avatar size={48} src={avatarUrl} userName={userName} />
-          <Typography>{userName}</Typography>
-        </div>
-      )}
+      <div className={s.userInfo}>
+        <Avatar size={48} src={avatarUrl} userName={userName || ''} />
+        <Typography>{userName}</Typography>
+      </div>
     </div>
   )
 }
