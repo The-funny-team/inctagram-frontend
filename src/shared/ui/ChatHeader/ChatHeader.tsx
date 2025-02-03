72caa3e06df99ada5bad32d2bdb3e9888aa1667d
Aleksandrlitvinov <84243297+Aleksandrlitvinov@users.noreply.github.com>
2025-02-03 01:25:07 +0100

    FUN-110 feat: add ChatHeader component

---
+++ src/shared/ui/ChatHeader/ChatHeader.tsx
@@ -0,0 +1,19 @@
+import { Avatar, Typography } from '@/shared/ui'
+
+import s from './ChatHeader.module.scss'
+
+type PropsType = {
+  avatarUrl: null | string
+  userName: string
+}
+
+export const ChatHeader = ({ avatarUrl, userName }: PropsType) => {
+  return (
+    <div className={s.headerWrapper}>
+      <div className={s.userInfo}>
+        <Avatar size={48} src={avatarUrl} userName={userName} />
+        <Typography>{userName}</Typography>
+      </div>
+    </div>
+  )
+}
