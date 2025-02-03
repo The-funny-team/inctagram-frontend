2cdf76cff0c699ab6cfc95e386f34402955a5295
Aleksandrlitvinov <84243297+Aleksandrlitvinov@users.noreply.github.com>
2025-02-03 01:37:48 +0100

    FUN-110 feat: add ChatHeader story

---
+++ src/shared/ui/ChatHeader/ChatHeader.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { ChatHeader } from './ChatHeader'
+
+const meta = {
+  component: ChatHeader,
+  tags: ['autodocs'],
+  title: 'Components/ChatHeader',
+} satisfies Meta<typeof ChatHeader>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    avatarUrl:
+      'https://staging-it-incubator.s3.eu-central-1.amazonaws.com/trainee-instagram-api/Image/4164019e-a863-4222-ab5b-e2657e8a8bb2_users/848/avatar/c309d797-46fe-4711-89a3-0349d083982f-images-192x192',
+    userName: 'Aleksandr_23',
+  },
+}
+
+export const Empty: Story = {}
--- src/shared/ui/ChatHeader/ChatHeader.tsx
+++ src/shared/ui/ChatHeader/ChatHeader.tsx
@@ -3,17 +3,19 @@ import { Avatar, Typography } from '@/shared/ui'
 import s from './ChatHeader.module.scss'
 
 type PropsType = {
-  avatarUrl: null | string
-  userName: string
+  avatarUrl?: null | string
+  userName?: null | string
 }
 
 export const ChatHeader = ({ avatarUrl, userName }: PropsType) => {
   return (
     <div className={s.headerWrapper}>
-      <div className={s.userInfo}>
-        <Avatar size={48} src={avatarUrl} userName={userName} />
-        <Typography>{userName}</Typography>
-      </div>
+      {userName && avatarUrl && (
+        <div className={s.userInfo}>
+          <Avatar size={48} src={avatarUrl} userName={userName} />
+          <Typography>{userName}</Typography>
+        </div>
+      )}
     </div>
   )
 }
