8f171c9280738f9c8747313c81e1d3af9e5fac76
Sergey <Zaikouski.sergey@gmail.com>
2025-02-16 13:29:01 +0200

    FUN-113 feat: add props and requests

--- src/widgets/Messenger/Chat/Chat.tsx
+++ src/widgets/Messenger/Chat/Chat.tsx
@@ -1,25 +1,65 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
+import { SocketEvent, useConnectSocket } from '@/shared/api/hooks/useConnectSocket'
+import { MessageViewModel, useGetMessagesByUserQuery } from '@/shared/api/messengerApi'
+import SocketApi from '@/shared/api/socket-api'
 import { PictureMessageIcon, VoiceMessageIcon } from '@/shared/assets'
 import { useTranslation } from '@/shared/lib/hooks'
-import { Input, Typography } from '@/shared/ui'
+import { Input, Message, Typography } from '@/shared/ui'
 
 import s from './Chat.module.scss'
 
-export const Chat = () => {
+type Props = {
+  avatarUrl: string
+  currentUserId: number
+  userName: string
+}
+
+export const Chat = ({ avatarUrl, currentUserId, userName }: Props) => {
   const { text } = useTranslation()
   const t = text.pages.messenger.chat
   const [newMsg, setNewMsg] = useState('')
-  const [messages, setMessages] = useState([])
+  const [messages, setMessages] = useState<MessageViewModel[]>([])
 
   const onNewMsgChange = (text: string) => {
     setNewMsg(text)
+  }
+  const { data: currentMessages } = useGetMessagesByUserQuery({ dialoguePartnerId: currentUserId })
+  const { message: newMessage } = useConnectSocket()
+
+  useEffect(() => {
+    if (currentMessages?.items.length) {
+      const newMessages = [...currentMessages.items].reverse()
+
+      setMessages(newMessages)
+    }
+    if (newMessage) {
+      setMessages(prevState => [...prevState, newMessage])
+    }
+  }, [currentMessages, newMessage])
+
+  const sentMessageHandler = () => {
+    SocketApi.socket?.emit(SocketEvent.RECEIVE_MESSAGE, {
+      message: newMsg,
+      receiverId: currentUserId,
+    })
+    setNewMsg('')
   }
 
   return (
     <div className={s.chatWrapper}>
       <div className={s.chat}>
-        {!messages.length && (
+        {messages && messages.length > 0 ? (
+          messages.map(mes => (
+            <Message
+              avatarUrl={avatarUrl}
+              currentUserId={currentUserId}
+              key={mes.id}
+              message={mes}
+              userName={userName}
+            />
+          ))
+        ) : (
           <Typography className={s.emptyChat} variant={'mediumText14'}>
             {t.noChosenChat}
           </Typography>
@@ -27,6 +67,7 @@ export const Chat = () => {
       </div>
       <div className={s.inputWrapper}>
         <Input
+          disabled={!currentUserId}
           onValueChange={onNewMsgChange}
           placeholder={t.input.placeholder}
           type={'text'}
@@ -34,7 +75,7 @@ export const Chat = () => {
         />
         <div className={s.inputActions}>
           {newMsg ? (
-            <Typography className={s.inputSend} variant={'boldText16'}>
+            <Typography className={s.inputSend} onClick={sentMessageHandler} variant={'boldText16'}>
               {t.input.action}
             </Typography>
           ) : (
